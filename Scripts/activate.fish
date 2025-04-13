2ae077e566bedf8afc9a15f29e8821d4721f9b86
Angela Ivonne Romo Criollo <airomoc@sanmateo.edu.co>
2025-04-12 20:58:39 -0500

    Creacion tablas Oficinas y Usuarios

--- Scripts/activate.fish
+++ Scripts/activate.fish
@@ -33,11 +33,11 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\Darin\Documents\django-p\mv-cto-prod'
+set -gx VIRTUAL_ENV 'C:\Users\Darin\Documents\django-d\mv-cto-dev'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
-set -gx VIRTUAL_ENV_PROMPT mv-cto-prod
+set -gx VIRTUAL_ENV_PROMPT mv-cto-dev
 
 # Unset PYTHONHOME if set.
 if set -q PYTHONHOME
@@ -57,7 +57,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s(%s)%s " (set_color 4B8BBE) mv-cto-prod (set_color normal)
+        printf "%s(%s)%s " (set_color 4B8BBE) mv-cto-dev (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
--- cto_project/cto_project/settings.py
+++ cto_project/cto_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'paa.apps.PaaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
